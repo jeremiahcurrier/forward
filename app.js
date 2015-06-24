672327a4e7764939e6d6e706d79c3757bb504ec1
Jeremiah Currier <jeremiah.currier@gmail.com>
2015-06-23 22:31:38 -0700

    Adding all updated files for the v1 POC for Snapchat :tada:

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 (function() {
-// Mandrill API key
+// Mandrill API key below:
 // lTcREt9DBP2LPqR8E0WEEg
   return {
-
     events: {
-      'app.created': 'init',
-      'click .email': 'testEmail'
-
+      // Framework Events
+      'app.created'             : function() {
+        this.switchTo('main');
+      },
+      // DOM Events
+      'click .email-test'       : 'testEmail', // Send test email via Mandrill
+      'click .toggle-app'       : 'toggleAppContainer' // Open/Close element inside .toggle-app CSS selector
+      // Request Events (none yet)
     },
 
     requests: {
-
       mandrillAgentTEST: function(notificationData) {
-        var message = helpers.fmt('You were mentioned in ticket <a href="%@">#%@</a> and the comment is below.<br></br><br><i>"%@"</i>', notificationData['ticket_url'], notificationData['ticket_id'], notificationData['comment_text']);
-
-        var currentUser = this.currentUser();
-
+        var messageTemplate = this.renderTemplate('email', {notification: notificationData}),
+            currentUser = this.currentUser();
         return {
           url: 'https://mandrillapp.com/api/1.0/messages/send-template.json',
           type: 'POST',
@@ -25,35 +26,42 @@
             "template_content": [
               {
                 "name": "body",
-                "content": message
+                "content": messageTemplate
               }
             ],
             "message": {
-                "subject": "Holla! You were mentioned in ticket #" + notificationData['ticket_id'],
-                "from_email": "jeremiah@zendesk.com",
-                "from_name": "Test Forwarding Notification From Zendesk App",
+                "subject": "This is a forward of the contents from Zendesk Ticket #" + notificationData['ticket_id'],
+                "from_email": currentUser.email(),
+                "from_name": currentUser.name(),
                 "to": [
                   {
-                    "email": currentUser.email(),
-                    "name": currentUser.name(),
+                    "email": notificationData['toEmail'],
+                    "name": "",
                     "type": "to"
                   }
                 ],
                 "headers": {
-                  "Reply-To": "noreply@zendesk.com"
+                  "Reply-To": "snapchatTEST@zendesk.com"
                 }
             }
           }
         };
       }
-
-    },
-
-    init: function() {
-      this.switchTo('main');
     },
 
     testEmail: function() {
+      // Get TO email address & optional comment (if present)
+      var input = this.$('input').val();
+      var comment = this.$('textarea').val();
+
+      // Store TO email address & optional comment (if present) in localStorage
+      this.store('forwardAppToEmail', input);
+      this.store('forwardAppOptionalComment', comment);
+
+      // Check if TO email address is present
+      if (this.store('forwardAppToEmail') === '') {
+        services.notify('Did you forget to enter an email address before clicking save?', 'alert');
+      } else {
       var ticket           = this.ticket(),
           ticket_id        = ticket.id(),
           currentAccount   = this.currentAccount(),
@@ -61,21 +69,42 @@
           notificationData = {
             'comment_text' : 'TEST COMMENT TEXT',
             'ticket_id'    : ticket_id,
-            'ticket_url'   : url + '/requests/' + ticket_id
+            'ticket_url'   : url + '/requests/' + ticket_id,
+            'toEmail'      : this.store('forwardAppToEmail')
           };
-
-      this.ajax('mandrillAgentTEST', notificationData)
-        .done( function(response) {
-          console.log('######## testEmail - DONE');
-          console.log(response);
-          this.switchTo('response', {
-            status: response[0].status
+      if (this.store('forwardAppToEmail') === null) {
+        services.notify('Please enter the email address you want to send this ticket to', 'error');
+      } else {
+        this.ajax('mandrillAgentTEST', notificationData) // pass notificationData to renderTemplate and use that variable as 
+          .done( function(data) { // looks like the .done is firing no matter what? 
+            console.log('response:');
+            console.log(data.status);
+            console.log('done');
+            this.switchTo('main');
+            services.notify('Ticket forwarded to <strong>' + this.store('forwardAppToEmail') + '</strong> successfully!', 'notice');
+            this.store('forwardAppToEmail', null); // clear out stored toEmail value
           })
-        })
-        .fail( function(response) {
-          console.log('######## testEmail - FAIL');
-          console.log(response);
-        });
+          .fail( function(data) {
+            console.log('response:');
+            console.log(data.status);
+            console.log('fail');
+            services.notify('Ticket failed to be forwarded to <strong>' + this.store('forwardAppToEmail') + '</strong>', 'error');
+            this.store('forwardAppToEmail', null); // clear out stored toEmail value
+          });
+        }
+      }
+    },
+
+    toggleAppContainer: function(){ // ZOMG you can totes collapse ur app now hon!
+      var $container  = this.$('.app-container'),
+          $icon       = this.$('.toggle-app i');
+      if ($container.is(':visible')){
+        $container.hide();
+        $icon.prop('class', 'icon-plus');
+      } else {
+        $container.show();
+        $icon.prop('class', 'icon-minus');
+      }
     }
 
   };
